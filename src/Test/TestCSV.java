3d790e1459562a71bcf9d3d55dfa976760ff9900
j99590314 <j99590314@gmail.com>
2015-04-13 09:50:53 +0800

    done Lab1

--- src/Test/TestCSV.java
+++ src/Test/TestCSV.java
@@ -1,5 +1,27 @@
 package Test;
 
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.io.CSV;;
+
 public class TestCSV {
 
+	@Test
+	public void testReadCategoryDataset() throws IOException
+	{
+		CSV csv = new CSV();
+		StringReader stringReader = new StringReader(" , A,B\nC,1,2\nD,3,4");
+		CategoryDataset categoryDataset = csv.readCategoryDataset(stringReader);
+		Assert.assertEquals(2, categoryDataset.getColumnKeys().size());
+		Assert.assertEquals(2, categoryDataset.getRowKeys().size());
+		Assert.assertEquals(1.0, categoryDataset.getValue(0, 0));
+		Assert.assertEquals(2.0, categoryDataset.getValue(0, 1));
+		Assert.assertEquals(3.0, categoryDataset.getValue(1, 0));
+		Assert.assertEquals(4.0, categoryDataset.getValue(1, 1));
+	}
 }
--- src/Test/TestRange.java
+++ src/Test/TestRange.java
@@ -1,5 +1,109 @@
 package Test;
 
-public class TestRange {
+import org.jfree.data.Range;
+import org.junit.Assert;
+import org.junit.Test;
 
+public class TestRange {
+	@Test
+	public void testCombine_WithTwoSameRange()
+	{
+		Range aRange1 = new Range(0, 0);
+		Range aRange2 = new Range(0, 0);
+		Range result = Range.combine(aRange1, aRange2);
+		Assert.assertEquals(0, result.getLength(), 0.000001);
+		Assert.assertEquals(0, result.getLowerBound(), 0.000001);
+		Assert.assertEquals(0, result.getUpperBound(), 0.000001);
+		Assert.assertEquals(0, result.getCentralValue(), 0.000001);
+	}
+	
+	@Test
+	public void testCombine_WithTwoIntersectRange()
+	{
+		Range aRange1 = new Range(-1, 1);
+		Range aRange2 = new Range(-0.5, 5);
+		Range result = Range.combine(aRange1, aRange2);
+		Assert.assertEquals(6, result.getLength(), 0.000001);
+		Assert.assertEquals(-1, result.getLowerBound(), 0.000001);
+		Assert.assertEquals(5, result.getUpperBound(), 0.000001);
+		Assert.assertEquals(2, result.getCentralValue(), 0.000001);
+	}
+	
+	@Test
+	public void testCombine_WithTwoUnintersectRange()
+	{
+		Range aRange1 = new Range(-1, 0);
+		Range aRange2 = new Range(0, 9);
+		Range result = Range.combine(aRange1, aRange2);
+		Assert.assertEquals(10, result.getLength(), 0.000001);
+		Assert.assertEquals(-1, result.getLowerBound(), 0.000001);
+		Assert.assertEquals(9, result.getUpperBound(), 0.000001);
+		Assert.assertEquals(4, result.getCentralValue(), 0.000001);
+	}
+	
+	@Test
+	public void testConstrain()
+	{
+		Range aRange = new Range(-1, 1);
+		Assert.assertEquals(1, aRange.constrain(1), 0.000001);
+		Assert.assertEquals(-1, aRange.constrain(-1), 0.000001);
+		Assert.assertEquals(0, aRange.constrain(0), 0.000001);
+		Assert.assertEquals(0.1, aRange.constrain(0.1), 0.000001);
+		Assert.assertEquals(-0.1, aRange.constrain(-0.1), 0.000001);
+		Assert.assertEquals(1, aRange.constrain(99), 0.000001);
+		Assert.assertEquals(-1, aRange.constrain(-99), 0.000001);
+	}
+	
+	@Test
+	public void testContains()
+	{
+		Range aRange = new Range(-1, 1);
+		Assert.assertFalse(aRange.contains(-1.1));
+		Assert.assertTrue(aRange.contains(-1));
+		Assert.assertTrue(aRange.contains(0));
+		Assert.assertTrue(aRange.contains(1));
+		Assert.assertFalse(aRange.contains(1.1));
+	}
+	
+	@Test
+	public void testGetCentralValue_WithNegativeRange()
+	{
+		Range aRange = new Range(-10, -5);
+		Assert.assertEquals(-7.5, aRange.getCentralValue(), 0.000001);
+	}
+	
+	@Test
+	public void testGetCentralValue_WithNegativeAndPositiveRange()
+	{
+		Range aRange = new Range(-8, 6);
+		Assert.assertEquals(-1, aRange.getCentralValue(), 0.000001);
+	}
+	
+	@Test
+	public void testGetCentralValue_WithPositiveRange()
+	{
+		Range aRange = new Range(1, 6);
+		Assert.assertEquals(3.5, aRange.getCentralValue(), 0.000001);
+	}
+	
+	@Test
+	public void testGetLength_WithNegativeRange()
+	{
+		Range aRange = new Range(-10, -5);
+		Assert.assertEquals(5, aRange.getLength(), 0.000001);
+	}
+	
+	@Test
+	public void testGetLength_WithNegativeAndPositiveRange()
+	{
+		Range aRange = new Range(-8, 6);
+		Assert.assertEquals(14, aRange.getLength(), 0.000001);
+	}
+	
+	@Test
+	public void testGetLength_WithPositiveRange()
+	{
+		Range aRange = new Range(1, 6);
+		Assert.assertEquals(5, aRange.getLength(), 0.000001);
+	}
 }
