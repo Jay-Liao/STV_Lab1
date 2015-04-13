61d9d4beab4e1c1a6acef5d404d3f22ab37fb6f5
Jay <j99590314@gmail.com>
2015-04-13 23:16:25 +0800

    CSV OK!

--- src/Test/TestCSV.java
+++ src/Test/TestCSV.java
@@ -23,4 +23,18 @@ public class TestCSV {
 		Assert.assertEquals(3.0, categoryDataset.getValue(1, 0));
 		Assert.assertEquals(4.0, categoryDataset.getValue(1, 1));
 	}
+	
+	@Test
+	public void testReadCategoryDataset_WithTextDelimiter() throws IOException
+	{
+		CSV csv = new CSV();
+		StringReader stringReader = new StringReader(" , A,B\nC,\"1\",\"2\"\nD,\"3\",\"4\"");
+		CategoryDataset categoryDataset = csv.readCategoryDataset(stringReader);
+		Assert.assertEquals(2, categoryDataset.getColumnKeys().size());
+		Assert.assertEquals(2, categoryDataset.getRowKeys().size());
+		Assert.assertEquals(1.0, categoryDataset.getValue(0, 0));
+		Assert.assertEquals(2.0, categoryDataset.getValue(0, 1));
+		Assert.assertEquals(3.0, categoryDataset.getValue(1, 0));
+		Assert.assertEquals(4.0, categoryDataset.getValue(1, 1));
+	}
 }
